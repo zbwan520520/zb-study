6174cd916cfd7cb6f95e81a47a58c1f5f16e7675
zhangbing <758888232@qq.com>
2022-04-01 20:46:58 +0800

    commit6

--- springcloud-project/my-spring-cloud/git-test3/src/main/java/com/hyron/GitTest3Application.java
+++ springcloud-project/my-spring-cloud/git-test3/src/main/java/com/hyron/GitTest3Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitTest3Application {
 
-    // git test5
+    // git test6
     public static void main(String[] args) {
         SpringApplication.run(GitTest3Application.class, args);
     }
